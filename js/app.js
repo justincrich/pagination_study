3c91f9c90687b1fa6da27b45c422f2aba4b4255f
Justin Rich <justinrich2008@gmail.com>
2017-07-11 15:16:45 -0700

    updated comments

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
+//Pagination variables
 var start = 0;
 var end = 10;
 var list =$('.student-list li').hide();
 var pages =$('.student-list li').length/10;
 $(`.student-list li`).slice(start,end).show();
+//setup pagination buttons
 for(var i = 0;i<pages;i++){
   $('.pagination ul').append(`<li>
     <a class='' href="#">${i+1}</a>
   </li>`);
 }
-
-//setup initial page for pagination buttons
 var page= 0;
 $(`.pagination li`).eq(page).children().addClass('active');
 
@@ -18,10 +18,14 @@ $(document).ready(function(){
   //click handlers for pages buttons
   $('.pagination li').on('click',(e)=>{
     e.preventDefault();
+    //reset search input field
     $('#searchInput').val('');
+    //hide old list
     $('.student-list li').slice(start,end).hide();
+    //set new end/start range
     end = $(e.target).text() *10;
     start = end-10;
+    //show list items that correspond with the start end range
     $(`.student-list li`).slice(start,end).show();
     $(`.pagination li`).eq(page).children().toggleClass('active');
     page = $(e.target).text() - 1;
@@ -33,6 +37,8 @@ $(document).ready(function(){
 
 
 });
+
+//function that controls the search feature of the page ... use typeahead
 function search (){
 
   //setup variables
